237970572f593075b199fd6b612f01aab3f61be4
kidus <kidusabebe101@gmail.com>
2023-08-31 08:35:44 +0300

    Implement the user registration route

---
+++ server/routes/users.js
@@ -0,0 +1,31 @@
+// User Registration Process
+
+const router = require("express").Router();
+const {User , validate } = require("../models/user")
+const bcrypt = require("bcrypt")
+
+router.post("/" , async(req , res)=> {
+    try{
+        const { error } = validate(req.body);
+        if(error){
+            return res.status(400).send({message : error.details[0].message})
+        }
+        const user = await User.findOne({email : req.body.email });
+
+        if (user){
+            return res.status(409).send({ message : "User with the given email already exists" })
+        }
+
+        const salt = await bcrypt.genSalt(Number(process.env.SALT))
+        const hashPassword = await bcrypt.hash(req.body.password,salt)
+
+        await new User({...req.body ,password:hashPassword })
+        res.status(201).send({message : " User Created Successfullly "})
+
+    }catch(error){
+        res.status(500).send({message : "Internal Server Error"})
+    }
+})
+
+
+module.exports = router;
